93082a8217e6d454f99ce996f14bbd93feeeda33
hyeong1 <mi75625@gmail.com>
2023-06-29 13:47:02 +0900

    아이디/비밀번호 세션 전송

--- js/user.js
+++ js/user.js
@@ -4,12 +4,10 @@ const nextToPlay = document.querySelector('#next-done');
 
 if (nextToPlay === null) {
     nextToPw.addEventListener('click', (event) => {
-        // check name
         window.location = 'signIn.html';
     });
 } else {
     nextToPlay.addEventListener('click', (event) => {
-        // check pw
         window.location = 'gameStart.html';
     });
 }
