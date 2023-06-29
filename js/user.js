ba4237c248534286e764c203210f5f17e23dcace
hyeong1 <mi75625@gmail.com>
2023-06-29 09:14:51 +0900

    login page update

--- js/user.js
+++ js/user.js
@@ -1,1 +1,15 @@
 /* signIn, signUp */
+const nextToPw = document.querySelector('#next');
+const nextToPlay = document.querySelector('#next-done');
+
+if (nextToPlay === null) {
+    nextToPw.addEventListener('click', (event) => {
+        // check name
+        window.location = 'signIn.html';
+    });
+} else {
+    nextToPlay.addEventListener('click', (event) => {
+        // check pw
+        window.location = 'gameStart.html';
+    });
+}
